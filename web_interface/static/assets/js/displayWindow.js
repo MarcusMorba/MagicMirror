6cddb81832375523d259f8e1a43f14bc6d655ad4
smalleight17 <smalleight17@gmail.com>
2018-03-29 13:45:38 -0400

    add sub-timeline under root timeline

--- web_interface/static/assets/js/displayWindow.js
+++ web_interface/static/assets/js/displayWindow.js
@@ -1,13 +1,15 @@
 
 function windowTimeline(){
   var tl_circle = displayCircle(300,600,200);
-  //var tl_triangle = displayTriangle();
+  var tl_triangle = displayTriangle();
+  var tl_spotlight = displaySpotlight();
+  var tl_square = displayRect();
 
-  // var root_timeline = new TimelineMax();
-  // root_timeline.add(function(){
-  //   displayCircle(300,600,200);
-  // }, 2);
-  // root_timeline.add(displayTriangle, 4);
+  var root_timeline = new TimelineMax({repeat: 3});
+  root_timeline.add(tl_circle);
+  root_timeline.add(tl_triangle, 36)
+  root_timeline.add(tl_spotlight, 62);
+  root_timeline.add(tl_square, 82);
 }
 
 
@@ -32,6 +34,7 @@ function displayCircle(pos_x, pos_y, size){
     radius: 5,
     angle: 20,
     x: size + 20,
+    opacity: 0,
     isShowStart:true
   });
   
@@ -76,35 +79,27 @@ function displayCircle(pos_x, pos_y, size){
   var arcInSVG = arc_in.el.childNodes[0].childNodes[0];
   var arcOutSVG = arc_out.el.childNodes[0].childNodes[0];
 
-  var beforeShow = new TimelineMax();
-  beforeShow.to(circle.el, 5, {opacity:1}, 0)
-            .to(arc_out.el, 1, {rotation: -90} , 0)
-            .to(dot_out.el, 1, {opacity: 1}, 1);
+  var tl1 = new TimelineMax();
+  tl1.to(circle.el, 5, {opacity:1}, 0)
+     .to(arc_out.el, 1, {rotation: -90} , 0)
+     .to([arc_in.el, arc_out.el, dot_in.el, dot_out.el], 1, {opacity: 1}, 1);
 
 
-  var tl = new TimelineMax({repeat: 1, repeatDelay: 6, onComplete:function(){
+  var tl2 = new TimelineMax({repeat: 1, repeatDelay: 6, onComplete:function(){
     TweenMax.to([circle.el, arc_in.el, arc_out.el], 1, {opacity: 0});
-    // setTimeout(function(){
-    //   try{
-    //     document.body.removeChild(circle.el);
-    //     document.body.removeChild(arc_in.el);
-    //     document.body.removeChild(arc_out.el);
-    //     displayTriangle(); 
-    //   }catch(err){
-    //     console.log("err from displayCircle");
-    //   }
-    // }, 3000);
-    
-}});
+  }});
 
-  tl.to(arc_in.el, 6, {rotation: "-=1080", ease: Sine.easeInOut}, 5)
+  tl2.to(arc_in.el, 6, {rotation: "-=1080", ease: Sine.easeInOut}, 5)
     .to(arcInSVG, 2, {strokeDashoffset: 1000, ease: Sine.easeIn}, 5)
     .to(arc_out.el, 6, {rotation: "-=1080", ease: Sine.easeInOut}, 5)
     .to(arcOutSVG, 2, {strokeDashoffset: 1000, ease: Sine.easeIn}, 5)
     .to(arcInSVG, 2, {strokeDashoffset: 1382.300767579509  , ease: Sine.easeOut},11)
     .to(arcOutSVG, 2, {strokeDashoffset: 1507.9644737231006  , ease: Sine.easeOut}, 11);
 
-  return tl;
+  var root_tl = new TimelineMax();
+  root_tl.add(tl1);
+  root_tl.add(tl2, 2);
+  return root_tl;
 }
 
 
@@ -120,28 +115,20 @@ function displayTriangle(){
     isShowStart: true,
   });
   
-  TweenMax.to(triangle.el, 0.5, {opacity: 1});
-
+ 
   var tl = new TimelineMax({repeat:1, repeatDelay:3, onComplete:function(){
     TweenMax.to(triangle.el, 0.5, {opacity: 0}, 3);
     flashLights();
-  //   setTimeout(function(){
-  //     try{
-  //       document.body.removeChild(triangle.el);
-  //       displaySpotlight(); 
-  //     }catch(err){
-  //       console.log("err from displayTriangle");
-  //     }
-      
-     
-  //   }, 3000);
    }});
   tl.to(triangle.el, 0.5, {rotation: 90}, 1)
     .to(triangle.el, 0.5, {rotation: 180}, 4)
     .to(triangle.el, 0.5, {rotation:270},7 )
     .to(triangle.el, 0.5, {rotation:360}, 10);
   
-  return tl;
+  var root_tl =  new TimelineMax();
+  root_tl.add(TweenMax.to(triangle.el, 0.5, {opacity: 1}));
+  root_tl.add(tl, 1);
+  return root_tl;
 
 }
 
@@ -157,25 +144,15 @@ function displaySpotlight(){
     isShowStart: true,
   });
   
-  var tl = new TimelineMax({repeat: 1, repeatDelay: 2, onComplete:function(){
-    setTimeout(function(){
-      try{
-        document.body.removeChild(circle.el);
-        displayRect();
-      }catch(err){
-        console.log("err from displaySpotlight");
-      }
-    
-     
-    }, 3000);
-  }});
-
+  var tl = new TimelineMax({repeat:1, repeatDelay:2});
   tl.to(circle.el, 0.5, {scale:0})
     .to(circle.el, 0.5, {scale: 1, opacity: 1})
     .to(circle.el, 0.5, {left:"-=120px", top:"+=150px"}, 2.5)
     .to(circle.el, 0.5, {left:"+=120px", top:"+=150px"}, 5)
     .to(circle.el, 0.5, {scale: 0}, 7.5);
+  return tl;
 }
+
 
 function displayRect(){
   var square = new mojs.Shape({
@@ -184,25 +161,17 @@ function displayRect(){
       radius: 10,
       left: 300, 
       top: 600,
-      //opacity: 0,
+      opacity: 0,
       isShowStart: true
   });
 
   var squareSVG = square.el.childNodes[0].childNodes[0];
-  var tl = new TimelineMax({onComplete: function(){
-    setTimeout(function(){
-      try{
-        document.body.removeChild(square.el);
-        displayCircle(300,600,200);
-      }catch(err){
-        console.log("err from displayRect")
-      }
-    
-     
-    }, 3000);
-  }});
 
-  tl.to([square.el, squareSVG], 1, {width: 400, height: 400, left:"-=150px", top:"-=150px"})
+  var tl = new TimelineMax();
+  tl.to([square.el], 0.5, {opacity: 1})
+    .to([square.el, squareSVG], 1, {width: 400, height: 400, left:"-=150px", top:"-=150px"},0.5)
     .to(square.el, 0.5, {width: 50, left:"+=350px"}, 10)
     .to(square.el, 0.5, {height:0, top:"-=150px"});
+
+  return tl;
 }
